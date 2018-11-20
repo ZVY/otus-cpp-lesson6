99fc89ea5ecc6b5378cfa1c95fc23fc0c1748469
ZVY <zvymailbox@gmail.com>
2018-11-20 20:39:55 +0300

    Modified CustomAllocator 4

--- CustomAllocator.h
+++ CustomAllocator.h
@@ -8,7 +8,7 @@ class CustomAllocator
 {
 	public:
 	    using size_type = size_t;
-		using difference_type = ptrdiff_t;
+		using difference_type = std::ptrdiff_t;
 		using pointer = T*;
 		using const_pointer = const T*;
 		using reference = T&;
